14282d110f4b33e936a291657d82489333a24b85
Daniel Müller <xdanielspagex@gmail.com>
2015-01-27 11:01:10 +0100

    Incredients im vereinbarten Format

--- backend/javascript/ean.js
+++ backend/javascript/ean.js
@@ -28,15 +28,15 @@ var exports = {
 					var jsonData = {};
 					
 					//Clear all values
-					jsonData["Name"] = "";
-					jsonData["Type"] = "";
-					jsonData["Vendor"]= "";
-					jsonData["incredients"] = "";
+					jsonData["name"] = "";
+					jsonData["type"] = "";
+					jsonData["vendor"]= "";
+					jsonData["contents"] = "";
 
 					// Set all values
-					jsonData["Name"] = lDetailname;
-					jsonData["Type"] = ltype;
-					jsonData["Vendor"]= lvendor;
+					jsonData["name"] = lDetailname;
+					jsonData["type"] = ltype;
+					jsonData["vendor"]= lvendor;
 
 
 					//Handle incredients
@@ -51,14 +51,14 @@ var exports = {
 					lstart = ltext.indexOf("\n")+2;
 					ltext = ltext.substring(lstart, ltext.length);
 					
-					jsonData["incredients"] = [];
+					jsonData["contents"] = [];
 
 					while (ltext.length > 0){
 						// Auf Subkategorie überprüfen
 						if ((ltext.indexOf(":") > (ltext.indexOf(" "))) && (ltext.indexOf(":") > (ltext.indexOf("\n"))) || (ltext.indexOf(":") == -1)){
 							var lincredient = {};
 							// Incredient bauen
-						 	lincredient["Name"] = ltext.substring(0, ltext.indexOf(" "));
+						 	lincredient["name"] = ltext.substring(0, ltext.indexOf(" "));
 						 	ltext = ltext.substring(ltext.indexOf(" ")+1, ltext.length);
 
 						 	// Auf Ungleich-Zeichen achten
@@ -66,14 +66,14 @@ var exports = {
 								ltext = ltext.substring(ltext.indexOf("<")+2, ltext.length);
 							}
 
-						 	lincredient["Amount"] = ltext.substring(0, ltext.indexOf(" "));
+						 	lincredient["amount"] = ltext.substring(0, ltext.indexOf(" "));
 						 	ltext = ltext.substring(ltext.indexOf(" ")+1, ltext.length);
 
 						 	if (ltext.indexOf("\n") == -1){
-						 		lincredient["Unit"] = ltext;
+						 		lincredient["unit"] = ltext;
 						 		ltext = "";
 						 	}else{
-						 		lincredient["Unit"] = ltext.substring(0, ltext.indexOf("\n"));
+						 		lincredient["unit"] = ltext.substring(0, ltext.indexOf("\n"));
 						 		ltext = ltext.substring(ltext.indexOf("\n")+1, ltext.length);
 						 	}
 					 	
@@ -82,8 +82,6 @@ var exports = {
 							ltext = ltext.substring(ltext.indexOf("\n")+1, ltext.length);
 						}
 					} 
-
-
 
 
 		        	// Return JSON 
