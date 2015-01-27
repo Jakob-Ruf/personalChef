4c9846e826bd0785a50232fb95b147db74ce08dc
Ras Pi <jakob.ruf93+bitbucket@gmail.com>
2015-01-27 11:14:17 +0100

    Diesmal werden die nicht mehr benötigten Dateien hoffentlich wirklich gelöscht

--- backend/config/database.js
+++
@@ -1,7 +0,0 @@
-// config/database.js
-module.exports = {
-
-    'url' : 'mongodb://Daniel:penopeno@proximus.modulusmongo.net:27017/a9bywedA' 
-    // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
-
-};
