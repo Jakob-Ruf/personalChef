adc9c8253353c3d3ea26fb069f55fd2780fba06a
Daniel Müller <xdanielspagex@gmail.com>
2015-01-28 11:08:57 +0100

    Bugfix AGB Controller

--- app/agb/agb.js
+++ app/agb/agb.js
@@ -8,12 +8,7 @@ angular.module('rezeptApp.agb', ['ngRoute'])
 		reloadOnSearch: true});
 }])
 
-.controller('AgbController',['$scope','agb', function()
+.controller('AgbController',['$scope', function($scope)
 {
-
-
-
-
-
 
 }]);
